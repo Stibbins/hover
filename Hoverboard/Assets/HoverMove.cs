632cd7db7bf46c38da347601906a4731625139b7
Stibbins <john.spjuth@gmail.com>
2014-06-07 16:57:49 +0200

    trying out some things

---
+++ Hoverboard/Assets/HoverMove.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoverMove : MonoBehaviour {
+
+	public bool moving;
+	
+	[SerializeField] private float sideMultiplier;
+	[SerializeField] private float forwardMultiplier;
+	
+	private float zRotation; //Strafe
+	private float xRotation; //Forward & Backward
+	
+	private Quaternion rotation;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		zRotation = Input.GetAxisRaw("RightHorizontal") * sideMultiplier; 
+		xRotation = Input.GetAxisRaw("Triggers") * forwardMultiplier;
+		if (zRotation != 0 || xRotation != 0)
+		{
+			moving = true;
+		}
+		else
+		{
+			moving = false;
+		}
+	}
+	
+	void FixedUpdate()
+	{
+		rotation = Quaternion.Euler(new Vector3 (xRotation, 0, zRotation));
+		
+		
+		rigidbody.MoveRotation(rotation);
+	}
+	
+	
+}
---
+++ Hoverboard/Assets/Hoverforce.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hoverforce : MonoBehaviour {
+
+	[SerializeField] private float forceMultiplier;
+	
+	[SerializeField] private float forceDistanceMulitplier;
+	
+	[SerializeField] private float hoverDistance;
+	
+	[SerializeField] private float distanceForce;
+	
+	private bool moveBoost;
+	
+	private float moveBoostAmount;
+	
+	
+	void Start()
+	{
+		rigidbody.centerOfMass.Set(transform.position.x, transform.position.y - 0.5f, transform.position.z);
+	}
+
+	void Update()
+	{
+		moveBoost = transform.GetComponent<HoverMove>().moving;
+		if (moveBoost)
+		{
+			moveBoostAmount = 1.5f;
+		}
+		else
+		{
+			moveBoostAmount = 1;
+		}
+	}
+
+	void FixedUpdate()
+	{
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position, -transform.up, out hit, hoverDistance))
+		{
+			distanceForce = forceMultiplier * (-1*(hoverDistance - (hoverDistance/hit.distance)));
+			rigidbody.AddForceAtPosition(transform.up*distanceForce*moveBoostAmount,transform.position, ForceMode.VelocityChange);
+		}
+	}
+}
